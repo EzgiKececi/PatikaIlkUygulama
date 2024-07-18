5975b394b598ba28df76b8091cf2de46a3b2b36b
Ezgi Kececi Atalay <ezgikececii@gmail.com>
2024-07-18 08:30:18 +0300

    Add files via upload

---
+++ PatikaIlkUygulama/PatikaIlkUygulama/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IlkUygulama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ezgi Keçeci Atalay");
+            Console.WriteLine("Yazılım sektöründe etkin bir rol alabilmek için Patika+ ile kendimi geliştirmek istiyorum.");
+        }
+    }
+}
